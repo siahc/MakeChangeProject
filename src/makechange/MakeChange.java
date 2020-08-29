0bfd960b445d2e82e0c2f9558e521c1ab9f099cb
Josiah Candler <josiahcandler@Josiahs-MacBook-Pro.local>
2020-08-28 18:21:43 -0600

    finished getting user input

--- src/makechange/MakeChange.java
+++ src/makechange/MakeChange.java
@@ -1,7 +1,32 @@
 package makechange;
 
+import java.util.Scanner;
+
 public class MakeChange {
 	public static void main(String[] args) {
+		
+		Scanner kb = new Scanner(System.in);
+		
+		double price, tendered, change;
+		
+		System.out.println("What is the price of the item? ");
+		
+		price = kb.nextDouble();
+		System.out.println("How much did you pay");
+		tendered = kb.nextDouble();
+		
+		
+		
+		
+		
+		
+		
+				
+				
+				
+				
+				
+				kb.close();
 		
 	}
 }
